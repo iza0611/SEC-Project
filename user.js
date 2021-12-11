732544a85d67f0b3dc12f92f41f142a1d2ff026b
iza0611 <95975374+iza0611@users.noreply.github.com>
2021-12-11 21:47:57 +0500

    Add files via upload

---
+++ user.js
@@ -0,0 +1,23 @@
+function openMulOpt(){
+	location.href="multicompiler.html";
+}
+
+function openJavaCom(){
+	location.href="javacompiler.html";
+}
+
+function openCCom(){
+	location.href="ccompiler.html";
+}
+
+function openPyCom(){
+	location.href="pythoncompiler.html";
+}
+
+function openSignin(){
+	location.href="signin.html";
+}
+
+function openSignup(){
+	location.href="signup.html"
+}
